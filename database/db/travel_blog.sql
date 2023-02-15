2a339362724a9e9e70b5d5752716ddd682bb2014
Árpád Szaniszló <developedbybarley@gmail.com>
2023-02-15 16:53:36 +0100

    sql schema upload

--- database/db/travel_blog.sql
+++ database/db/travel_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Feb 14. 17:39
+-- Létrehozás ideje: 2023. Feb 15. 16:52
 -- Kiszolgáló verziója: 10.4.27-MariaDB
 -- PHP verzió: 8.0.25
 
@@ -38,7 +38,29 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `email`, `password`) VALUES
-(2, 'Barley@gmail.com', '$2y$10$UqKKJne5NsLtCBzq6mR69udIoE8LbU6H8.e5uk2VToofSCOgdoJ3G');
+(4, 'Barley@gmail.com', '$2y$10$wjHdZ8YrO1vU2etz73dY5OMPPBRdPkzCN2SAqjZJlmYA1M2rDKVr.');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `trips`
+--
+
+CREATE TABLE `trips` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `adminId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `trips`
+--
+
+INSERT INTO `trips` (`id`, `title`, `description`, `time`, `adminId`) VALUES
+(1, 'Első trip', 'Első trip description test!', 123132, NULL),
+(2, 'Trip 2 test!', 'Trip description again !TEST!', 3213123, NULL);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -51,6 +73,13 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- A tábla indexei `trips`
+--
+ALTER TABLE `trips`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `adminId` (`adminId`);
+
+--
 -- A kiírt táblák AUTO_INCREMENT értéke
 --
 
@@ -58,7 +87,23 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT a táblához `admin`
 --
 ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT a táblához `trips`
+--
+ALTER TABLE `trips`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `trips`
+--
+ALTER TABLE `trips`
+  ADD CONSTRAINT `fk1` FOREIGN KEY (`adminId`) REFERENCES `admin` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
